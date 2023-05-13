0b91f769fdf1cb0ae2c2b9d796105802566d9014
James <james@eli.cx>
2023-05-13 18:10:27 +0100

    use CF_PAGES_URL for base url

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,7 +1,10 @@
 import { cache } from 'react';
 
+const addHttps = (url: string) =>
+  url.startsWith('http') ? url : `https://${url}`;
+
 export const getBaseUrl = cache(() =>
-  process.env.VERCEL_URL
-    ? `https://app-dir.vercel.app`
+  process.env.CF_PAGES_URL
+    ? addHttps(process.env.CF_PAGES_URL)
     : `http://127.0.0.1:${process.env.PORT ?? 8788}`,
 );
