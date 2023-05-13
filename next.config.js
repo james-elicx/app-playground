892c597295cb5313c7dbf169bcc9e52c933e8195
James <james@eli.cx>
2023-05-13 16:31:33 +0100

    edge runtime + redirects/rewrites/middleware

--- next.config.js
+++ next.config.js
@@ -1,9 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
+  runtime: 'experimental-edge',
   experimental: {
     appDir: true,
   },
+  rewrites: async () => ({
+    beforeFiles: [
+      // These rewrites are checked after headers/redirects
+      // and before all files including _next/public files which
+      // allows overriding page files
+      {
+        source: '/some-page',
+        destination: '/somewhere-else',
+        has: [{ type: 'query', key: 'overrideMe' }],
+      },
+    ],
+    afterFiles: [
+      // These rewrites are checked after pages/public files
+      // are checked but before dynamic routes
+      {
+        source: '/non-existent',
+        destination: '/somewhere-else',
+      },
+    ],
+    fallback: [
+      // These rewrites are checked after both pages/public files
+      // and dynamic routes are checked
+      {
+        source: '/:path*',
+        destination: `https://my-old-site.com/:path*`,
+      },
+    ],
+  }),
+  redirects: async () => [
+    {
+      source: '/about',
+      destination: '/',
+      permanent: true,
+    },
+  ],
+  // i18n: {
+  //   locales: ['en', 'fr'],
+  //   defaultLocale: 'en',
+  // },
+  // basePath: '/base-path',
 };
 
 module.exports = nextConfig;
