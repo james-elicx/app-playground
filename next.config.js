ef9ade781f234100a450edd64d981f79c22ecc20
James <james@eli.cx>
2023-05-16 11:12:27 +0100

    remove redirects/rewrites

--- next.config.js
+++ next.config.js
@@ -7,41 +7,41 @@ const nextConfig = {
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  rewrites: async () => ({
-    beforeFiles: [
-      // These rewrites are checked after headers/redirects
-      // and before all files including _next/public files which
-      // allows overriding page files
-      {
-        source: '/some-page',
-        destination: '/somewhere-else',
-        has: [{ type: 'query', key: 'overrideMe' }],
-      },
-    ],
-    afterFiles: [
-      // These rewrites are checked after pages/public files
-      // are checked but before dynamic routes
-      {
-        source: '/non-existent',
-        destination: '/somewhere-else',
-      },
-    ],
-    fallback: [
-      // These rewrites are checked after both pages/public files
-      // and dynamic routes are checked
-      {
-        source: '/:path*',
-        destination: `https://my-old-site.com/:path*`,
-      },
-    ],
-  }),
-  redirects: async () => [
-    {
-      source: '/about',
-      destination: '/',
-      permanent: true,
-    },
-  ],
+  // rewrites: async () => ({
+  //   beforeFiles: [
+  //     // These rewrites are checked after headers/redirects
+  //     // and before all files including _next/public files which
+  //     // allows overriding page files
+  //     {
+  //       source: '/some-page',
+  //       destination: '/somewhere-else',
+  //       has: [{ type: 'query', key: 'overrideMe' }],
+  //     },
+  //   ],
+  //   afterFiles: [
+  //     // These rewrites are checked after pages/public files
+  //     // are checked but before dynamic routes
+  //     {
+  //       source: '/non-existent',
+  //       destination: '/somewhere-else',
+  //     },
+  //   ],
+  //   fallback: [
+  //     // These rewrites are checked after both pages/public files
+  //     // and dynamic routes are checked
+  //     {
+  //       source: '/:path*',
+  //       destination: `https://my-old-site.com/:path*`,
+  //     },
+  //   ],
+  // }),
+  // redirects: async () => [
+  //   {
+  //     source: '/about',
+  //     destination: '/',
+  //     permanent: true,
+  //   },
+  // ],
   // i18n: {
   //   locales: ['en', 'fr'],
   //   defaultLocale: 'en',
